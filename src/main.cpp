c14befa12399924e4e6871ab9c0546cb51f6b043
Mark Bennett <mbennett.em@gmail.com>
2026-04-21 16:26:41 -0600

    Get it working on Windows.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include <wx/wx.h>
 #include <fmt/core.h>
 
+
+
+
 class MyApp : public wxApp {
 public:
-    bool OnInit() override {
-        fmt::print("App starting...\n");
-
-        wxFrame* frame = new wxFrame(nullptr, wxID_ANY, "Hybrid wxWidgets App");
-        frame->Show(true);
-
-        return true;
-    }
+    virtual bool OnInit();
 };
 
 wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit() {
+
+    fmt::print("App starting...\n");
+    wxFrame* frame = new wxFrame(nullptr, wxID_ANY, "Hybrid wxWidgets App");
+    frame->Show(true);
+
+    return true;
+}
